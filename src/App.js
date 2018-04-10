f44543cbb9e59aeded9803d92695b6df113bcab9
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-04-10 10:53:49 -0500

    frist commiot

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+import pokes from './pokes.json';
+import { Card, CardImg, CardBody,
+  CardTitle, Row, Col } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Pokemon List</h1>
+        </header>
+        <Pokemon />
+      </div>
+    );
+  }
+}
+
+class Pokemon extends Component{
+  render(){
+  return (
+    <div className="Card-body">
+          <Row>
+            {
+              Object.keys(pokes).map((poke) => {
+                return (
+                <Col sm="4">
+                <Card className="Card">
+                  <CardImg top width="100%" src={pokes[poke].ThumbnailImage} alt="Card image cap" />
+                  <CardBody>
+                    <CardTitle>{pokes[poke].name}</CardTitle>
+                  </CardBody>
+                </Card>
+                </Col>
+               )
+              })
+            }
+          </Row>
+          </div>
+    )
+  }
+}
+
+
+export default App;
